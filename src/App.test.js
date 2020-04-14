3458e52b2ba843b3ddf6ffcd85e4938f777033a2
Pavan <pavantejakusunuri@gmail.com>
2020-04-14 06:21:57 +0530

    Successfully Added Tests for Congrats Components

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,35 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new EnzymeAdapter()})
+
+/**
+ * @function setup
+ * @param {object} props - Component props specific to this setup.
+ * @param {object} state - Initial state for setup
+ * @returns {ShallowWrapper} 
+ */
+
+const setup = (props={}, state=null) => {
+  const wrapper = shallow(<App {...props} />)
+  if (state) wrapper.setState(state)
+  return wrapper;
+}
+
+/** 
+ * Return ShallowWrapper containing node(s) with the given data-test value.
+ *  @param { ShallowWrapper } wrapper - Enzyme shallow wrapper to search within.
+ *  @param {string} val - Value of data-test attribute for search.
+ *  @returns { ShallowWrapper } 
+ */
+
+ const findByTestAttr = (wrapper, val) => {
+   return wrapper.find(`[data-test="${val}"]`);
+ }
+
+
+test('renders without error', () => {
 });
